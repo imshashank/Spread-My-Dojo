71ddf946f32933cdc64e4df6cfe23fc383d5d13e
root <shashank@shankee.com>
2014-02-08 11:59:40 +0400

    files

---
+++ send.php
@@ -0,0 +1,22 @@
+<?php
+require 'vendor/autoload.php';
+//Dotenv::load(__DIR__);
+
+$sendgrid_username = 'osuhack';
+$sendgrid_password = 'osu_hack1';
+$to                = 'shashankmailster@gmail.com';
+
+$sendgrid = new SendGrid($sendgrid_username, $sendgrid_password, array("turn_off_ssl_verification" => true));
+$email    = new SendGrid\Email();
+$email->addTo($to)->
+       setFrom($to)->
+       setSubject('[sendgrid-php-example] Owl')->
+       setText('Owl are you doing?')->
+       setHtml('<strong>%how% are you doing?</strong>')->
+       addSubstitution("%how%", array("Owl"))->
+       addMessageHeader('X-Sent-Using', 'SendGrid-API')->
+       addMessageHeader('X-Transport', 'web');
+//       addAttachment('./gif.gif', 'owl.gif');
+
+$response = $sendgrid->web->send($email);
+var_dump($response);
